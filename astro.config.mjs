b01793bbf162d3037ce0ebcc0f6062fb1a3c3268
Pablo Pérez <ppmoya08@gmail.com>
2024-12-04 12:14:30 -0300

    feat: add contact page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-import compress from "astro-compress";
+// import compress from "astro-compress";
 import vercelStatic from '@astrojs/vercel/static';
 
 import sitemap from "@astrojs/sitemap";
 
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pabloprx.tech',
-  integrations: [mdx(), compress(), sitemap()],
+  site: 'https://pablodev.cl',
+  integrations: [mdx(), sitemap(), tailwind({ applyBaseStyles: false}), react()],
   output: 'static',
   adapter: vercelStatic({
     analytics: true
